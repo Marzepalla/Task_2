f83c23e47bea0305ba2857721d4314bb71c26d05
Marzepalla <tklimoshenko@mail.ru>
2016-03-23 15:05:47 +0200

    Task 2

---
+++ Test_task_2/IntegerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task_2
+{
+    public static class IntegerExtensions
+    {
+        public static List<int> GetListOfDividers(this int number)
+        {
+            List<int> listDividers = new List<int> { 1 };
+
+            for (int i = 2;  i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    listDividers.Add(i);
+                }
+            }
+            listDividers.Add(number);
+            return listDividers;
+        }
+    }
+}
---
+++ Test_task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число ");
+            List<int> listOfDividers = Int32.Parse(Console.ReadLine()).GetListOfDividers();
+
+            Console.WriteLine("Целочисленные делители введённого числа:");
+            foreach (int elem in listOfDividers)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+   
+}
